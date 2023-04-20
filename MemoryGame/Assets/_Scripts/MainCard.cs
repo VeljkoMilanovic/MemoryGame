995a4541647465a198e7a63adf6609f9b14647da
Veljko Milanovic <83020328+VeljkoMilanovic@users.noreply.github.com>
2023-04-20 21:09:34 +0200

    Initial Commit

---
+++ MemoryGame/Assets/_Scripts/MainCard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour {
+
+    [SerializeField] private SceneController controller;
+    [SerializeField] private GameObject Card_Back;
+
+    private AudioSource cardFlipAudio;
+
+    private void Awake()
+    {
+        cardFlipAudio = GetComponent<AudioSource>();
+    }
+
+    public void OnMouseDown()
+    {
+        Reveal();
+    }
+
+    private void Reveal()
+    {
+        if (Card_Back.activeSelf && controller.canReveal)
+        {
+            cardFlipAudio.Play();
+
+            Card_Back.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void ChangeSprite(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void Unreveal()
+    {
+        Card_Back.SetActive(true);
+    }
+
+
+}
